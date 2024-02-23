0a8ed10a46383693483bf7272ceb70c87ac7c8c8
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-02-23 09:48:51 +0100

    hotovo

--- index.js
+++ index.js
@@ -2,16 +2,59 @@ const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
   description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
-};
+}
 
 const product2 = {
   image: 'images/slepickabelka.jpg',
   name: 'Slepičkabelka',
   description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
-};
+}
 
 const product3 = {
   image: 'images/anatoplavky.jpg',
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
-};
+}
+
+
+
+
+const product1HTML = document.querySelector("#product1")
+
+product1HTML.innerHTML = `
+<img class="card-img-top" src=${product1.image}  alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
+const product2HTML = document.querySelector("#product2")
+
+product2HTML.innerHTML = `
+  <img class="card-img-top" src=${product2.image} alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product2.name}</h2>
+    <p class="card-text">${product2.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
+const product3HTML = document.querySelector("#product3")
+
+product3HTML.innerHTML = `
+  <img class="card-img-top" src=${product3.image} alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product3.name}</h2>
+    <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
+
+const produkt1 = document.querySelector("#product1")
+produkt1.classList.add("border-primary")
+
+const button2 = document.querySelector("#product2 button")
+button2.classList.remove("btn-primary")
+
+const title = document.querySelector("#product3 h2")
+title.classList.toggle("text-center")
